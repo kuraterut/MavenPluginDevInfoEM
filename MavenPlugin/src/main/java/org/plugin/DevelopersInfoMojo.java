fd9edd0f15a1b88f598653553ac118f394d9a4b3
kuraterut <kurylilia@gmail.com>
2025-05-15 22:48:02 +0300

    feat: using Utils

--- MavenPlugin/src/main/java/org/plugin/DevelopersInfoMojo.java
+++ MavenPlugin/src/main/java/org/plugin/DevelopersInfoMojo.java
@@ -6,8 +6,11 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 @Mojo(name = "info")
 public class DevelopersInfoMojo extends AbstractMojo {
@@ -17,8 +20,8 @@ public class DevelopersInfoMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         List<Developer> developers = project.getDevelopers();
 
-        if (developers == null || developers.isEmpty()) {
-            getLog().info("No developers information found in the POM.");
+        if (CollectionUtils.isEmpty(developers)) {
+            getLog().warn("No developers information found in the POM.");
             return;
         }
 
@@ -27,29 +30,29 @@ public class DevelopersInfoMojo extends AbstractMojo {
 
         for (Developer developer : developers) {
             getLog().info("Developer:");
-            if (developer.getId() != null) {
-                getLog().info("  ID: " + developer.getId());
+            if (StringUtils.isNotEmpty(developer.getId())) {
+                getLog().info(String.format("  ID: %s", developer.getId()));
             }
-            if (developer.getName() != null) {
-                getLog().info("  Name: " + developer.getName());
+            if (StringUtils.isNotEmpty(developer.getName())) {
+                getLog().info(String.format("  Name: %s", developer.getName()));
             }
-            if (developer.getEmail() != null) {
-                getLog().info("  Email: " + developer.getEmail());
+            if (StringUtils.isNotEmpty(developer.getEmail()) ) {
+                getLog().info(String.format("  Email: %s", developer.getEmail()));
             }
-            if (developer.getUrl() != null) {
-                getLog().info("  URL: " + developer.getUrl());
+            if (StringUtils.isNotEmpty(developer.getUrl())) {
+                getLog().info(String.format("  URL: %s", developer.getUrl()));
             }
-            if (developer.getOrganization() != null) {
-                getLog().info("  Organization: " + developer.getOrganization());
+            if (StringUtils.isNotEmpty(developer.getOrganization())) {
+                getLog().info(String.format("  Organization: %s", developer.getOrganization()));
             }
-            if (developer.getOrganizationUrl() != null) {
-                getLog().info("  Organization URL: " + developer.getOrganizationUrl());
+            if (StringUtils.isNotEmpty(developer.getOrganizationUrl())) {
+                getLog().info(String.format("  Organization URL: %s", developer.getOrganizationUrl()));
             }
-            if (developer.getRoles() != null && !developer.getRoles().isEmpty()) {
-                getLog().info("  Roles: " + String.join(", ", developer.getRoles()));
+            if (CollectionUtils.isNotEmpty(developer.getRoles())) {
+                getLog().info(String.format("  Roles: %s", String.join(", ", developer.getRoles())));
             }
-            if (developer.getTimezone() != null) {
-                getLog().info("  Timezone: " + developer.getTimezone());
+            if (StringUtils.isNotEmpty(developer.getTimezone())) {
+                getLog().info(String.format("  Timezone: %s", developer.getTimezone()));
             }
             getLog().info("--------------------------------");
         }
